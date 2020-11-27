8168264de75f585a3a9480b7643dcce848153687
Sandro Meloni <sandromeloni02@gmail.com>
2020-11-27 16:18:36 +0100

    Aggiungere i file di progetto.

---
+++ CaesarCipher/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using CaesarCipherLibrary;
+
+namespace CaesarCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Inserisci una parola: ");
+                string plaintext = Console.ReadLine();
+                int key;
+                CaesarCode m1;
+                try
+                {
+                    do
+                    {
+                        Console.Write("Inserisci la chiave: ");
+                        key = int.Parse(Console.ReadLine());
+
+                        if (key < 0 || key > 25) Console.WriteLine("Chiave errata!");
+                    } while (key < 0 || key > 25);
+
+                    m1 = new CaesarCode(plaintext, key);
+                    Console.Write($"Il testo cifrato è: {m1.CipherText}");
+                }
+                catch
+                {
+                    Console.Write("Devi inserire un numero!");
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
